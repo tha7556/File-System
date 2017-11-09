bae72c516711494a300a27361bfbfd3586e75886
Tyler <tha7556@users.noreply.github.com>
2017-11-09 16:06:15 -0500

    Started init()

--- files.c
+++ files.c
@@ -26,10 +26,24 @@
 
 void files_init()
 {
+    int i;
+    for(i = 0; i < MAX_OPENFILE; i++) {
+        theDirectory[i].free = true;
+        theDirectory[i].filename = NULL;
+        theDirectory[i].inode = &inodesTbl[i];
+        inodesTbl[i].inode_id = i;
+    }
+    for(i = 0; i < MAX_DEV; i++) {
+        Dev_Tbl[i].num_of_free_blocks = MAX_BLOCK;
+        int j;
+        for(j = 0; j < MAX_BLOCK; j++) {
+            Dev_Tbl[i].free_blocks[j] = true;
+        }
+    }
 }
 
 
-
+//good luck
 void openf(char *filename, OFILE *file)
 {
 }
@@ -56,7 +70,7 @@ EXIT_CODE writef(OFILE *file, int position, int page_id, IORB *iorb)
 
 void notify_files(IORB *iorb)
 {
-} 
+}
 
 
 
@@ -73,7 +87,7 @@ int search_file(char *filename)
 
 
 int new_file(char *filename)
-{ 
+{
 }
 
 
